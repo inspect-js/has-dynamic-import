6a1ac5209ced527eea97e529b9b9de7752fa62b8
Linus Miller <linus.miller@bitmill.io>
2021-05-14 13:55:17 +0200

    [New] add `syntax` entry point that matches current main entry point
    Co-authored-by: Linus Miller <linus.miller@bitmill.io>
    Co-authored-by: Jordan Harband <ljharb@gmail.com>

--- README.md
+++ README.md
@@ -11,11 +11,14 @@
 
 Does the current environment have `import()` support?
 
-At the time of this writing, node v10+ has support for dynamic [`import()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import#dynamic_imports);
+At the time of this writing, node v12.17+, v13.2+ and v14.0+ has support for dynamic [`import()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import#dynamic_imports); the syntax is supported in node v10+.
 
-This library exports `true` if the current environment supports it, and `false` if it does not.
+The former can be checked in the package‘s main export, which is an `async function` that returns a boolean indicating if the current environment fully supports dynamic import. Note that in node v12.17 - v12.19 and v13.4 - v13.13, an ExperimentalWarning will be logged.
+
+The latter can be checked with `'has-dynamic-import/syntax'`, which is a function that returns a boolean indicating if the current environment supports parsing the syntax of dynamic import.
 
 ## Tests
+
 Simply clone the repo, `npm install`, and run `npm test`
 
 [package-url]: https://npmjs.org/package/has-dynamic-import
--- test/index.js
+++ test/index.js
@@ -2,7 +2,13 @@
 
 var test = require('tape');
 var semver = require('semver');
+var isBrowser = typeof window !== 'undefined';
+var isNode = !isBrowser && typeof process !== 'undefined';
+
 var hasDynamicImport = require('../');
+var hasSyntax = require('../syntax');
+
+var browserHasSyntax = require('../browser-syntax');
 
 test('hasDynamicImport', function (t) {
 	t.equal(typeof hasDynamicImport, 'function', 'is a function');
@@ -10,18 +16,39 @@ test('hasDynamicImport', function (t) {
 	var yes = hasDynamicImport();
 	t.equal(typeof yes, 'boolean', 'returns a boolean');
 
-	t.test('browser', { todo: true, skip: typeof window === 'undefined' }, function (st) {
-		st.end();
-	});
-
-	t.test('node', { skip: typeof global === 'undefined' }, function (st) {
-		st.equal(
-			yes,
-			semver.satisfies(process.version, '>= 10'),
-			'result matches expected node version range'
-		);
+	t.test('browser', { todo: true, skip: !isBrowser }, function (st) {
 		st.end();
 	});
 
 	t.end();
+});
+
+test('hasSyntax', function (t) {
+	t.equal(typeof hasSyntax, 'function', 'is a function');
+
+	t.test('node', { skip: !isNode }, function (st) {
+		var result = hasSyntax();
+
+		st.equal(typeof result, 'boolean', 'returns a boolean');
+		st.equal(
+			result,
+			semver.satisfies(process.version, '>=10'),
+			'result matches expected node version range'
+		);
+
+		st.end();
+	});
+
+	t.test('browser', function (st) {
+		var result = browserHasSyntax();
+
+		st.equal(typeof result, 'boolean', 'returns a boolean');
+		st.equal(
+			result,
+			hasSyntax(),
+			'matches result from node implementation'
+		);
+
+		st.end();
+	});
 });
